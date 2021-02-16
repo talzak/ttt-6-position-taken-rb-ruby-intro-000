46011ed537b6bcbceb17c931cd9ad0f13acb56ae
Tal Zak <tal.zak26@gmail.com>
2021-02-16 18:57:53 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] != " " || board[index] != ""
     return false
-  elif board[index] == "X" || board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     return true
   end
 end
